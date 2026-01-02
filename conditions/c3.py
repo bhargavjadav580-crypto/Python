25628b34e584fcfb9356e7291aef6db65dbf1c77
bhargavjadav580-crypto <bhargavjadav580@gmail.com>
2026-01-02 15:35:13 +0530

    add

---
+++ conditions/c3.py
@@ -0,0 +1,19 @@
+# nested if
+# nested loop
+
+age = int(input("Enter a number:"))
+
+if age>=18:
+	voter_id = input("Do you have voter id?")
+	if voter_id == 'yes':
+		print("Yes you can vote.")
+	else:
+		print("No you can't vote.")
+else:
+	print("You are not eligible for vote.")
+
+
+1. if num is div by 5 then check also div by 3 or not.
+2. check num is div by 3 if is not then check 7 .
+
+
